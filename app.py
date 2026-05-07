01858a6c44276c3c5e980ccca32ea8a725d14269
saskiyya <saskiahumaira02@gmail.com>
2026-05-07 22:59:15 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 df = pd.read_csv("dataset_gaji_data_science.csv")
 
 ######################################################### JUDUL ##################################################################
